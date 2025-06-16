54b39fcd8ac662586d74c225315e7b4333056514
najmus-shakib01 <syednajmusshakib130@gmail.com>
2025-06-17 04:00:40 +0600

    secoend commit

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True                       
 
-ALLOWED_HOSTS = ["crud-operation-mn15.onrender.com", "localhost"]
+ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOWED_ORIGINS = [
     "https://crud-operation-mn15.onrender.com",
