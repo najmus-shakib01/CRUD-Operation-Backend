8926b89b2565eaa40d28b0d06dea5e87e1a5812d
najmus-shakib01 <syednajmusshakib130@gmail.com>
2025-06-17 04:17:29 +0600

    feat(ALLOWED_HOSTS): refector

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -15,7 +15,11 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True                       
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    'crud-operation-pgbe.onrender.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://crud-operation-pgbe.onrender.com",
